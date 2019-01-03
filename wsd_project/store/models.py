931230746d030a8a10d48bfa5a3164a636e039b6
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2019-01-03 12:55:06 +0200

    testing templates

--- wsd_project/store/models.py
+++ wsd_project/store/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    name = models.CharField(unique=True, max_length=30)
+    price = models.FloatField()
+    link = models.URLField(unique=True)
+    dev_name = models.CharField(max_length=150)
+    tags = models.CharField() # JSON format
