3935763f783e313735a95516bcb7c5dfce342dae
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2019-01-03 22:38:44 +0200

    still trying to make the db work

--- wsd_project/wsd_project/settings.py
+++ wsd_project/wsd_project/settings.py
@@ -10,11 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
-import os
-import dj-database-url
+import os 
+import dj_database_url
 
-db_from_env = dj_database_url.config()
- DATABASES[‘default’].update(db_from_env)
+DATABASES = { 'default': dj_database_url.config() }
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -85,7 +84,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
