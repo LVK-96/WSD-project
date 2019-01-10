89a5986c4de1690b6eae2a255c00ebc1fe44c16f
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2019-01-10 11:19:52 +0200

    made refactored signup, started working on form to edit user details

--- wsd_project/users/forms.py
+++ wsd_project/users/forms.py
@@ -1,8 +1,15 @@
+'''
+Forms for user signup and update
+'''
+
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser  
+from .models import CustomUser, Profile
 
 class SignupForm(UserCreationForm):
+    '''
+    Form for user signup, profile created with signal in signals.py
+    '''
     class Meta(UserCreationForm):
         model = CustomUser
         fields = ('username', 'email', 'is_dev')
@@ -12,6 +19,17 @@ class SignupForm(UserCreationForm):
         }
 
 class ChangeForm(UserChangeForm):
+    '''
+    Form for user update
+    '''
     class Meta:
         model = CustomUser
-        fields = ('username', 'email', 'is_dev')
+        fields = ('username', 'email')
+
+class ProfileUpdateForm(forms.ModelForm):
+    '''
+    Form for profile update
+    '''
+    class Meta:
+        model = Profile
+        fields = ['profile_pic', 'bio']
--- wsd_project/users/views.py
+++ wsd_project/users/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django.views import generic
 from .models import CustomUser
-from .forms import SignupForm
+from .forms import SignupForm, ChangeForm, ProfileUpdateForm
 
-class SignUp(generic.CreateView):
-    form_class = SignupForm
-    success_url = reverse_lazy('login')
-    template_name = 'users/signup.html'
+def signup(request):
+    form = SignupForm
+    return render(request, 'users/signup.html', {'form': form})
 
+@login_required
 def profilepage(request):
+    u_form = ChangeForm
+    p_form = ProfileUpdateForm
     username = request.user.username
     user = CustomUser.objects.get(username=username)
+    
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    
     return render(request, 'users/profile.html', {"user":user})
+
+    
