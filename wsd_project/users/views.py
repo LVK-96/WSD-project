aa754bc279d6458e492b870abef74b109f61d49e
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2019-01-10 17:52:41 +0200

    fixd signup

--- wsd_project/users/views.py
+++ wsd_project/users/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
 from django.urls import reverse_lazy
 from django.views import generic
 from .models import CustomUser
 from .forms import SignupForm, ChangeForm, ProfileUpdateForm
 
 def signup(request):
-    form = SignupForm
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()  # load the profile instance created by the signal
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignupForm()
     return render(request, 'users/signup.html', {'form': form})
 
 @login_required
