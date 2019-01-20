f108d20a5f79e7f584324ae94d640c06656f5617
Jaakko Koskela <koskelj8@aalto.fi>
2019-01-20 22:25:58 +0200

    Revert "added user field to highscore - had to delete database to make migrations"
    This reverts commit 880459619c300dcff9ebda6a8d694a8a800429a2.

--- wsd_project/store/models.py
+++ wsd_project/store/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 from users.models import CustomUser
 
+#games are added to the database through forms
+#requires one model, one form and one view to function together
 #primary key -> can't be changed afterwards, name does not need to be set as a primary key
+#Game (name PRIMARY KEY, dev FOREIGN KEY, price, link, purchases, tags)
+#one game can have only one developer
+#how to check and limit that only developers can add a game
+#->restricting only developers access to this (form creation view)view might not be enough 
+#Game (name PRIMARY KEY, dev FOREIGN KEY, price, link, purchases, tags)
+#how to check that the user is actually a dev?, and how to set a dev to the game?
 
 class Game(models.Model):
     name = models.CharField(max_length=50)
@@ -13,10 +21,7 @@ class Game(models.Model):
 
 class Highscore(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
-    player = models.ForeignKey(CustomUser, on_delete=models.CASCADE) #owner of the game/highscore
     score = models.IntegerField(default=0, unique=False)
-    class Meta:
-        unique_together = ('game', 'player')
 
 class Order(models.Model):
     #buyer 
