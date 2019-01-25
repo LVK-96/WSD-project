fb0a3c87b35262dc0096f753923d5b9e32cf0821
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2019-01-25 13:02:42 +0200

    added payment urls and

--- wsd_project/store/urls.py
+++ wsd_project/store/urls.py
@@ -5,10 +5,14 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('games', views.games, name='games'),
+    path('store', views.store, name='store'),
+    path('library', views.my_library, name='my_library'),
     path('highscores', views.highscores, name='highscores'),
     path('addgame', views.addgame, name='addgame'),
     path('cart', views.cart, name='cart'),
-    path('highscore/<int:game_pk>/<int:new_score>', views.addhighscore, name='highscore')
+    path('highscore/<int:game_pk>/<int:new_score>', views.addhighscore, name='highscore'),
+    path('payment/success', views.payment_success, name='payment_success'),
+    path('payment/cancel', views.payment_cancel, name='payment_fail'),
+    path('payment/error', views.payment_error, name='payment_error')
 
 ]
