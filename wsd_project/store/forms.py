2660ce48cb3385fe33d0f9c8fc874e434773cec5
Henri-Matias Tuomaala <henri-matias.tuomaala@aalto.fi>
2019-02-18 15:41:18 +0200

    Added genre choises for games

--- wsd_project/store/forms.py
+++ wsd_project/store/forms.py
@@ -4,7 +4,7 @@ from .models import Game, Order
 class NewGameForm(forms.ModelForm):
     class Meta:
         model = Game
-        fields = ('name', 'price', 'link', 'description')
+        fields = ('name', 'price', 'link', 'description', 'genre')
 
 class GameUpdateForm(forms.ModelForm):
     class Meta:
