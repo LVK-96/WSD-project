8130ca3df37f71466c69ec4e8de1e320d4631311
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2019-02-18 18:29:37 +0200

    tweaked staticfiles settings

--- wsd_project/wsd_project/settings.py
+++ wsd_project/wsd_project/settings.py
@@ -170,6 +170,7 @@ EMAIL_USE_TLS = True
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "media"),
+    os.path.join(BASE_DIR, "media/profile_pics"),
     os.path.join(BASE_DIR, "store/static"),
 ]
 
